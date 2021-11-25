e9c805f9b8e4a6e210cf59e219894c1b62202410
20AAP02 <antoniomariabranches@gmail.com>
2021-11-25 19:35:36 +0000

    mistake in struct

--- ft_printf.c
+++ ft_printf.c
@@ -2,20 +2,43 @@
 #include <stdio.h> // official printf
 #include <stdarg.h>
 
+
+/* NOTAS
+
+order:
+1 - %
+2 - zero or more flags
+3 - optional minimum field width
+4 - optional precision
+5 - optional length modifier 
+
+*/
+
 typedef struct print_settings
 {
 	va_list args;
+	int hash;
+	int zero;
+	int dash;
+	int space;
+	int plus;
 	int width;
 	int precision;
-	int zero;
 	int point;
-	int dash;
+	int star;
 	int t_length;
-	int sign;
-	int is_zero;
-	int percentage;
-	int space_f;
 } str_settings;
+
+
+int	ft_isalpha(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
 
 str_settings *ft_initialise_tab(str_settings *tab)
 {
@@ -25,20 +48,46 @@ str_settings *ft_initialise_tab(str_settings *tab)
 	tab->point = 0;
 	tab->sign = 0;
 	tab->t_length = 0;
-	tab->is_zero = 0;
 	tab->dash = 0;
-	tab->percentage = 0;
-	tab->space_f = 0;
+	tab->space = 0;
 	return (tab);
+}
+
+int ft_analise_format(str_settings *tab, const char *format, int i)
+{
+	if (format[i] == '%')
+	{
+		write(1, &format[i], 1);
+		tab->t_length += 1;
+		return (i);
+	}
+	return (i);
 }
 
 int ft_printf(const char *format, ...)
 {
 	str_settings *tab;
+	int	i;
+	int	t_printed;
 
 	tab = (str_settings *)malloc(sizeof(tab));
 	if (!tab)
 		return (-1);
+	ft_initialise_tab(tab);
+	va_start(tab->args, format);
+	i = -1;
+	t_printed = 0;
+	while (format[++i])
+	{
+		if (format[i] == '%')
+			i = ft_analise_format(tab, format, i + 1);
+		else
+			t_printed += write(1, &format[i], 1);
+	}
+	va_end(tab->args);
+	t_printed += tab->t_length;
+	free(tab);
+	return(t_printed);
 }
 
 int main()
